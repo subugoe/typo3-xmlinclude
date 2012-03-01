88c04ed6cc29323844f9219c208c2cf2dfd8f15f
ssp <porst@sub.uni-goettingen.de>
2012-03-01 19:08:51 +0100

    many changes, first beta
    * refactor PHP code
    * add error handling
    * use fed extension
    * add settings for JavaScript / CSS inclusion
    * add ability to add URL parameters to the XML request
    * improve XSL
    * add Readme
    * add localisation

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'xmlinclude',
 	'description' => 'Loads, transforms and includes XML',
 	'category' => 'plugin',
-	'version' => '1.0.0',
+	'version' => '0.9.0',
 	'state' => 'alpha',
 	'author' => 'Sven-S. Porst',
 	'author_email' => 'porst@sub.uni-goettingen.de',
@@ -13,15 +13,16 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'php' => '5.3.0-0.0.0',
 			'typo3' => '4.6.4-0.0.0',
-			'extbase' => '1.3.0-0.0.0',
-			'fluid' => '1.3.0-0.0.0',
+			'extbase' => '1.4.2-0.0.0',
+			'fluid' => '1.4.1-0.0.0',
+			'fed' => '1.4.11-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'dependencies' => 'extbase,fluid',
+	'dependencies' => 'extbase,fluid,fed',
 	'conflicts' => '',
 	'suggests' => '',
 	'priority' => '',
