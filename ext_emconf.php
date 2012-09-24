f3c9881061c4853de9fe5e9cb27b6f50baa06a53
Sven-S. Porst <ssp-web@earthlingsoft.net>
2012-09-24 16:43:12 +0200

    upload v1.1.0 to TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "xmlinclude".
 #
-# Auto generated 02-05-2012 15:20
+# Auto generated 24-09-2012 16:42
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Include XML',
 	'description' => 'Loads, transforms and includes XML',
 	'category' => 'plugin',
-	'version' => '1.0.0',
+	'version' => '1.1.0',
 	'state' => 'stable',
 	'author' => 'Sven-S. Porst',
 	'author_email' => 'porst@sub.uni-goettingen.de',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'_md5_values_when_last_written' => 'a:15:{s:15:"README.markdown";s:4:"a51c";s:17:"ext_localconf.php";s:4:"e1d4";s:14:"ext_tables.php";s:4:"b06b";s:43:"Classes/Controller/XMLIncludeController.php";s:4:"6c55";s:41:"Classes/RealURL/tx_xmlinclude_realurl.php";s:4:"99ba";s:38:"Configuration/FlexForms/XMLInclude.xml";s:4:"9e1b";s:38:"Configuration/TypoScript/constants.txt";s:4:"d41d";s:34:"Configuration/TypoScript/setup.txt";s:4:"d371";s:49:"Resources/Private/Language/locallang-flexform.xml";s:4:"4e46";s:40:"Resources/Private/Language/locallang.xml";s:4:"cf90";s:49:"Resources/Private/Templates/XMLInclude/Index.html";s:4:"40e5";s:38:"Resources/Private/XSL/rewrite-urls.xsl";s:4:"711c";s:35:"Resources/Private/XSL/test/adw.html";s:4:"96b9";s:57:"Resources/Private/XSL/test/goescholar-community-list.html";s:4:"91fc";s:42:"Resources/Private/XSL/test/goescholar.html";s:4:"8b0c";}',
+	'_md5_values_when_last_written' => 'a:14:{s:17:"ext_localconf.php";s:4:"e1d4";s:14:"ext_tables.php";s:4:"b06b";s:15:"README.markdown";s:4:"88e6";s:43:"Classes/Controller/XMLIncludeController.php";s:4:"fd38";s:41:"Classes/RealURL/tx_xmlinclude_realurl.php";s:4:"4a65";s:38:"Configuration/FlexForms/XMLInclude.xml";s:4:"acc9";s:34:"Configuration/TypoScript/setup.txt";s:4:"8926";s:49:"Resources/Private/Language/locallang-flexform.xml";s:4:"4e46";s:40:"Resources/Private/Language/locallang.xml";s:4:"cf90";s:49:"Resources/Private/Templates/XMLInclude/Index.html";s:4:"9dde";s:38:"Resources/Private/XSL/rewrite-urls.xsl";s:4:"9e61";s:35:"Resources/Private/XSL/test/adw.html";s:4:"96b9";s:57:"Resources/Private/XSL/test/goescholar-community-list.html";s:4:"91fc";s:42:"Resources/Private/XSL/test/goescholar.html";s:4:"8b0c";}',
 );
 
 ?>
